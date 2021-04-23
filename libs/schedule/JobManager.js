8fb8425d63034758cb8aac51865f69696c024fd7
SeonghoJin <seongho4996@naver.com>
2021-04-24 01:40:36 +0900

    여러개의 Job을 관리하는 JobManager클래스 추가

---
+++ libs/schedule/JobManager.js
@@ -0,0 +1,23 @@
+const Job = require('./Job');
+
+class JobManager {
+
+    constructor() {
+        this.jobs = {};
+    }
+
+    addJob = (userId, hour, minute, callback) => {
+        this.jobs[userId] = new Job(hour, minute, callback);
+    }
+
+    rescheduleJob = (userId, hour, minute) => {
+        this.jobs[userId].reschedule(hour, minute);
+    }
+
+    changeJob = (userId, callback) => {
+        this.jobs[userId].changeJob(callback);
+    }
+
+}
+
+module.exports = new JobManager();
