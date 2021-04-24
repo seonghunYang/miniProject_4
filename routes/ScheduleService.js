df6bbba95693f42bc4c6f44f8c55861f1ba088e1
SeonghoJin <seongho4996@naver.com>
2021-04-24 16:52:01 +0900

    Schedule과 관련된 함수가 모인 ScheduleService.js파일 생성

---
+++ routes/ScheduleService.js
@@ -0,0 +1,19 @@
+const JobManager = require('../libs/schedule/JobManager');
+const libKakaoWork = require('../libs/kakaoWork');
+const view = require('../view');
+
+exports.send_app_install_message_all_users = async() => {
+    const users = await libKakaoWork.getUserList();
+    const conversations = await Promise.all(
+        users.map((user) => libKakaoWork.openConversations({ userId: user.id }))
+    );
+    const messages = await Promise.all([
+        conversations.map((conversation) =>
+            libKakaoWork.sendMessage({
+                conversationId: conversation.id,
+                ...view.app_install
+            })
+        ),
+    ]);
+    return { users, conversations, messages };
+}
