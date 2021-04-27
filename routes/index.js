19093e46a518234554c8428eba5d20c393461f04
SeonghoJin <seongho4996@naver.com>
2021-04-27 08:43:40 +0000

    코드 정리

--- routes/index.js
+++ routes/index.js
@@ -15,74 +15,82 @@ const keywords = require('../static/data/keywords')
 
 // routing 진입점
 router.get('/', async (req, res, next) => {
-  // 유저 목록 검색 (1)
-  const users = await libKakaoWork.getUserList();
-  const conversations = await Promise.all(
-    users.map((user) => libKakaoWork.openConversations({ userId: user.id }))
-  );
+    // 유저 목록 검색 (1)
+    const users = await libKakaoWork.getUserList();
+    const conversations = await Promise.all(
+        users.map((user) => libKakaoWork.openConversations({
+            userId: user.id
+        }))
+    );
 
-  // 생성된 채팅방에 메세지 전송 (3)
-  const messages = await Promise.all([
-    conversations.map((conversation) =>
-      libKakaoWork.sendMessage({
-        conversationId: conversation.id,
-		    ...view.bot_install_msg
-      })
-    ),
-  ])
-  // 응답값은 자유롭게 작성하셔도 됩니다.
-  res.json("OK");
+    // 생성된 채팅방에 메세지 전송 (3)
+    const messages = await Promise.all([
+        conversations.map((conversation) =>
+            libKakaoWork.sendMessage({
+                conversationId: conversation.id,
+                ...view.bot_install_msg
+            })
+        ),
+    ])
+    // 응답값은 자유롭게 작성하셔도 됩니다.
+    res.json("OK");
 });
 
 
 // 버튼 클릭시 survey 보내줌
 router.post('/request', async (req, res, next) => {
-  const { message, value } = req.body;
-  switch (value) {
-    case 'keyword_survey':
-      // 설문조사용 모달 전송
-      return res.json(view.keyword_survey());
-      break;
-	case 'time_select_modal':
-		return res.json(view.time_select_modal());
-		break;
-    default:
-  }
-  res.json("OK");
+    const {
+        message,
+        value
+    } = req.body;
+    switch (value) {
+        case 'keyword_survey':
+            // 설문조사용 모달 전송
+            return res.json(view.keyword_survey());
+            break;
+        case 'time_select_modal':
+            return res.json(view.time_select_modal());
+            break;
+        default:
+    }
+    res.json("OK");
 });
 
 
 // survey에 대한 callback
 router.post('/callback', async (req, res, next) => {
-  const { message, actions, action_time, value } = req.body; // 설문조사 결과 확인 (2)
-  crawler.crawling(actions.keyword_select).then((result) => {
-    
-    switch (value) {
-      
-		
-		case 'keyword_survey_results':
-        let keyword = keywords[actions.keyword_select];
-        const keyword_survey_results = view.keyword_survey_results(result, keyword)
-        libKakaoWork.sendMessage({
-          conversationId: message.conversation_id,
-          ...keyword_survey_results
-        })
-        break;
-		
-		
-		
-		case 'keyword_select_msg':
-        libKakaoWork.sendMessage({
-          conversationId: message.conversation_id,
-          ...view.keyword_select_msg()
-        })
-			break;
-      default:
-    }
-    res.json("OK");
-  }).catch(err => {
-    console.log(err)
-  })
+    const {
+        message,
+        actions,
+        action_time,
+        value
+    } = req.body; // 설문조사 결과 확인 (2)
+    crawler.crawling(actions.keyword_select).then((result) => {
+
+        switch (value) {
+				
+            case 'keyword_survey_results':
+                let keyword = keywords[actions.keyword_select];
+                const keyword_survey_results = view.keyword_survey_results(result, keyword)
+                libKakaoWork.sendMessage({
+                    conversationId: message.conversation_id,
+                    ...keyword_survey_results
+                })
+                break;
+
+            case 'keyword_select_msg':
+                libKakaoWork.sendMessage({
+                    conversationId: message.conversation_id,
+                    ...view.keyword_select_msg()
+                })
+                break;
+				
+            default:
+        }
+        res.json("OK");
+    }).catch(err => {
+        console.log(err)
+    })
 });
 
 module.exports = router;
--- view/index.js
+++ view/index.js
@@ -3,93 +3,97 @@ const header_template = require('../static/template/header');
 const keywords = require('../static/data/keywords');
 
 exports.bot_install_msg = {
-  "text": "키워드 뉴스 알림 봇을 설치해보세요!",
-  "blocks": [
-	header_template.header(""),
-    {
-      "type": "text",
-      "text": "안녕하세요. 저는 키워드 뉴스 알림⏰ 봇입니다! \n\n저를 설치하시면 설정하신 시간마다 관심 키워드에 관한 뉴스를 보실 수 있습니다. \n\n설치하실려면 시간설정버튼을 눌려주세요! ",
-      "markdown": true
-    },
-    {
-      "type": "button",
-      "text": "시간 설정",
-      "style": "primary",
-	  "action_type":'call_modal',
-	  "value" : "time_select_modal"
-    }]
+    "text": "키워드 뉴스 알림 봇을 설치해보세요!",
+    "blocks": [
+        header_template.header(""),
+        {
+            "type": "text",
+            "text": "안녕하세요. 저는 키워드 뉴스 알림⏰ 봇입니다! \n\n저를 설치하시면 설정하신 시간마다 관심 키워드에 관한 뉴스를 보실 수 있습니다. \n\n설치하실려면 시간설정버튼을 눌려주세요! ",
+            "markdown": true
+        },
+        {
+            "type": "button",
+            "text": "시간 설정",
+            "style": "primary",
+            "action_type": 'call_modal',
+            "value": "time_select_modal"
+        }
+    ]
 }
 
 exports.time_select_modal = () => {
-	return {view : {
-  "title": "뉴스 알림 시간",
-  "accept": "확인",
-  "decline": "취소",
-  "value": "keyword_select_msg",
-  "blocks": [
-    {
-      "type": "label",
-      "text": "키워드 뉴스를 보실 시간을 정해주세요!\n 정상적인 시간을 설정하지 않으시면 알람설정이 되지 않습니다!",
-      "markdown": true
-    },
-    {
-      "type": "input",
-      "name": "time",
-      "required": false,
-      "placeholder": "ex) 12:30"
+    return {
+        view: {
+            "title": "뉴스 알림 시간",
+            "accept": "확인",
+            "decline": "취소",
+            "value": "keyword_select_msg",
+            "blocks": [{
+                    "type": "label",
+                    "text": "키워드 뉴스를 보실 시간을 정해주세요!\n 정상적인 시간을 설정하지 않으시면 알람설정이 되지 않습니다!",
+                    "markdown": true
+                },
+                {
+                    "type": "input",
+                    "name": "time",
+                    "required": false,
+                    "placeholder": "ex) 12:30"
+                }
+            ]
+        }
     }
-  ]
-}}
-} 
+}
 
 exports.keyword_select_msg = () => {
-	return {
-    text: '키워드 고르기',
-    blocks: [
-		header_template.header(""),
-        {
-            type: 'text',
-            text: '어떤 뉴스를 보고 싶으신지\n 키워드를 골라주세요!😀',
-            markdown: true,
-        },
-        {
-            type: 'button',
-            action_type: 'call_modal',
-            value: 'keyword_survey',
-            text: '키워드 고르기',
-            style: 'primary',
-        },
-    ],}
+    return {
+        text: '키워드 고르기',
+        blocks: [
+            header_template.header(""),
+            {
+                type: 'text',
+                text: '어떤 뉴스를 보고 싶으신지\n 키워드를 골라주세요!😀',
+                markdown: true,
+            },
+            {
+                type: 'button',
+                action_type: 'call_modal',
+                value: 'keyword_survey',
+                text: '키워드 고르기',
+                style: 'primary',
+            },
+        ],
+    }
 }
 
 exports.keyword_survey = () => {
-	
-	const options = [];
-	
-	for(keyword_eng in keywords){
-		const keyword_kor = keywords[keyword_eng];
-		options.push({
-			text : keyword_kor,
-			value : keyword_eng
-		});
-	}
-	
-	return keyword_survey_template.keyword_survey(options);
+
+    const options = [];
+
+    for (keyword_eng in keywords) {
+        const keyword_kor = keywords[keyword_eng];
+        options.push({
+            text: keyword_kor,
+            value: keyword_eng
+        });
+    }
+
+    return keyword_survey_template.keyword_survey(options);
 }
 
 exports.keyword_survey_results = (result, keyword) => {
-	const ret = {
-		text: '키워드를 이용해 크롤링을 완료했습니다!',
-        blocks: [
-			header_template.header(keyword)
-		]
-	};
 	
-	for(let i = 0; i < 5; i++){
-		const title = result[i].title;
-		const url = result[i].url;
-		ret.blocks.push(keyword_survey_template.keyword_survey_result_item(i+1, title, url));
-	}
+    const ret = {
+        text: '키워드를 이용해 크롤링을 완료했습니다!',
+        blocks: [
+            header_template.header(keyword)
+        ]
+    };
 
-	return ret;
+    for (let i = 0; i < 5; i++) {
+        const title = result[i].title;
+        const url = result[i].url;
+        ret.blocks.push(keyword_survey_template.keyword_survey_result_item(i + 1, title, url));
+    }
+
+    return ret;
 }
